7f7216dc9e89a42cf26946e69733b9794b9e25c7
onlyliuxin <14703250@qq.com>
2018-05-09 15:22:19 +0800

    引入Exception

--- src/main/java/org/litespring/beans/factory/support/DefaultBeanFactory.java
+++ src/main/java/org/litespring/beans/factory/support/DefaultBeanFactory.java
@@ -11,6 +11,8 @@ import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 import org.litespring.beans.BeanDefinition;
+import org.litespring.beans.factory.BeanCreationException;
+import org.litespring.beans.factory.BeanDefinitionStoreException;
 import org.litespring.beans.factory.BeanFactory;
 import org.litespring.util.ClassUtils;
 
@@ -46,8 +48,8 @@ public class DefaultBeanFactory implements BeanFactory{
 				this.beanDefinitionMap.put(id, bd);
 			}
 		} catch (DocumentException e) {		
-			//TODO 抛出异常
-			e.printStackTrace();
+			throw new BeanDefinitionStoreException("IOException parsing XML document from " + configFile,e);
+
 		}finally{
 			if(is != null){
 				try {
@@ -69,24 +71,16 @@ public class DefaultBeanFactory implements BeanFactory{
 	public Object getBean(String beanID) {
 		BeanDefinition bd = this.getBeanDefinition(beanID);
 		if(bd == null){
-			return null;
+			throw new BeanCreationException("Bean Definition does not exist");
 		}
 		ClassLoader cl = ClassUtils.getDefaultClassLoader();
 		String beanClassName = bd.getBeanClassName();
 		try {
 			Class<?> clz = cl.loadClass(beanClassName);
 			return clz.newInstance();
-		} catch (ClassNotFoundException e) {			
-			e.printStackTrace();
-		} catch (InstantiationException e) {
-			
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			
-			e.printStackTrace();
-		}
-		
-		return null;
+		} catch (Exception e) {			
+			throw new BeanCreationException("create bean for "+ beanClassName +" failed",e);
+		} 
 	}
 
 }
--- src/test/java/org/litespring/test/v1/BeanFactoryTest.java
+++ src/test/java/org/litespring/test/v1/BeanFactoryTest.java
@@ -3,8 +3,11 @@ package org.litespring.test.v1;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.litespring.beans.BeanDefinition;
+import org.litespring.beans.factory.BeanCreationException;
+import org.litespring.beans.factory.BeanDefinitionStoreException;
 import org.litespring.beans.factory.BeanFactory;
 import org.litespring.beans.factory.support.DefaultBeanFactory;
 import org.litespring.service.v1.PetStoreService;
@@ -23,6 +26,28 @@ public class BeanFactoryTest {
 		PetStoreService petStore = (PetStoreService)factory.getBean("petStore");
 		
 		assertNotNull(petStore);
+	}
+	
+	@Test
+	public void testInvalidBean(){
+
+		BeanFactory factory = new DefaultBeanFactory("petstore-v1.xml");
+		try{
+			factory.getBean("invalidBean");
+		}catch(BeanCreationException e){
+			return;
+		}
+		Assert.fail("expect BeanCreationException ");
+	}
+	@Test
+	public void testInvalidXML(){
+		
+		try{
+			new DefaultBeanFactory("xxxx.xml");
+		}catch(BeanDefinitionStoreException e){
+			return;
+		}
+		Assert.fail("expect BeanDefinitionStoreException ");
 	}
 
 }
