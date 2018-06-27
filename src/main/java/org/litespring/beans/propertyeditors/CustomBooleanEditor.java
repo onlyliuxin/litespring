8eb96798a4061e1190d86b01269df0ea90257589
onlyliuxin <14703250@qq.com>
2018-06-27 18:24:33 +0800

    change "if" to "else if"

--- src/main/java/org/litespring/beans/propertyeditors/CustomBooleanEditor.java
+++ src/main/java/org/litespring/beans/propertyeditors/CustomBooleanEditor.java
@@ -36,7 +36,7 @@ public class CustomBooleanEditor extends PropertyEditorSupport {
 			// Treat empty String as null value.
 			setValue(null);
 		}
-		if ((VALUE_TRUE.equalsIgnoreCase(input) || VALUE_ON.equalsIgnoreCase(input) ||
+		else if ((VALUE_TRUE.equalsIgnoreCase(input) || VALUE_ON.equalsIgnoreCase(input) ||
 						VALUE_YES.equalsIgnoreCase(input) || VALUE_1.equals(input))) {
 			setValue(Boolean.TRUE);
 		}
